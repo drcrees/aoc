d9ff51eb8e1cf97eacbfd1de68affa5184fc8f0d
drcrees <dylan@crees.io>
2024-12-08 19:14:27 -0600

    fmt

--- 2024/08/p1/solution.go
+++ 2024/08/p1/solution.go
@@ -7,55 +7,55 @@ import (
 )
 
 type Coords struct {
-  x, y int
+	x, y int
 }
 
 type Antenna struct {
-  frequency rune
-  coords Coords
+	frequency rune
+	coords    Coords
 }
 
 type Antinode struct {
-  coords Coords
+	coords Coords
 }
 
 func Solve() {
 	fmt.Println("--- 8-1 ---")
-  grid := helpers.ReadRunes("./2024/08/p1/input")
+	grid := helpers.ReadRunes("./2024/08/p1/input")
 
-  antennas := findAntennas(grid)
+	antennas := findAntennas(grid)
 
-  result := 0
-  for f, a := range antennas {
-    for _, a1 := range a {
-      for _, a2 := range a {
-        an1, an2 := antinodes(a1.coords, a2.coords)
-        if inbounds(an1.x, an1.y, len(grid[0]), len(grid)) {
-          if grid[an1.y][an1.x] != f && grid[an1.y][an1.x] != '#' {
-            grid[an1.y][an1.x] = '#'
-            result++
-          }
-        }
-        if inbounds(an2.x, an2.y, len(grid[0]), len(grid)) {
-          if grid[an2.y][an2.x] != f && grid[an2.y][an2.x] != '#' {
-            grid[an2.y][an2.x] = '#'
-            result++
-          }
-        }
-      }
-    }
-  }
+	result := 0
+	for f, a := range antennas {
+		for _, a1 := range a {
+			for _, a2 := range a {
+				an1, an2 := antinodes(a1.coords, a2.coords)
+				if inbounds(an1.x, an1.y, len(grid[0]), len(grid)) {
+					if grid[an1.y][an1.x] != f && grid[an1.y][an1.x] != '#' {
+						grid[an1.y][an1.x] = '#'
+						result++
+					}
+				}
+				if inbounds(an2.x, an2.y, len(grid[0]), len(grid)) {
+					if grid[an2.y][an2.x] != f && grid[an2.y][an2.x] != '#' {
+						grid[an2.y][an2.x] = '#'
+						result++
+					}
+				}
+			}
+		}
+	}
 
 	fmt.Printf("Result: %d\n", result)
 }
 
 func antinodes(from Coords, to Coords) (Coords, Coords) {
-  delta := Coords{to.x-from.x, to.y-from.y}
+	delta := Coords{to.x - from.x, to.y - from.y}
 
-  a1 := Coords{delta.x*-1 + from.x, delta.y*-1 + from.y}
-  a2 := Coords{delta.x+to.x, delta.y+to.y}
+	a1 := Coords{delta.x*-1 + from.x, delta.y*-1 + from.y}
+	a2 := Coords{delta.x + to.x, delta.y + to.y}
 
-  return a1, a2
+	return a1, a2
 }
 
 func inbounds(x int, y int, maxX int, maxY int) bool {
@@ -66,32 +66,32 @@ func inbounds(x int, y int, maxX int, maxY int) bool {
 	return false
 }
 
-func findAntennas(grid [][]rune) map[rune][]*Antenna{
-  abf := make(map[rune][]*Antenna)
-  for y := 0; y < len(grid); y++ {
-    for x := 0; x < len(grid[y]); x++ {
-      if grid[y][x] != '.' {
-        antenna := &Antenna{
-          frequency: grid[y][x],
-          coords: Coords{x,y},
-        }
-        if abf[grid[y][x]] != nil {
-          abf[grid[y][x]] = append(abf[grid[y][x]], antenna)
-          continue
-        }
-        abf[grid[y][x]] = []*Antenna{antenna}
-      }
-    }
-  }
+func findAntennas(grid [][]rune) map[rune][]*Antenna {
+	abf := make(map[rune][]*Antenna)
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[y]); x++ {
+			if grid[y][x] != '.' {
+				antenna := &Antenna{
+					frequency: grid[y][x],
+					coords:    Coords{x, y},
+				}
+				if abf[grid[y][x]] != nil {
+					abf[grid[y][x]] = append(abf[grid[y][x]], antenna)
+					continue
+				}
+				abf[grid[y][x]] = []*Antenna{antenna}
+			}
+		}
+	}
 
-  return abf
+	return abf
 }
 
 func printGrid(grid [][]rune) {
-  for y := 0; y < len(grid); y++ {
-    for x := 0; x < len(grid[y]); x++ {
-      fmt.Printf("%c ", grid[y][x])
-    }
-    fmt.Println()
-  }
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[y]); x++ {
+			fmt.Printf("%c ", grid[y][x])
+		}
+		fmt.Println()
+	}
 }
