8dc5386070e16da2771d2e7a02ca40a998229c51
Dylan R Crees <CreesDylanR@JohnDeere.com>
2025-12-12 15:36:48 -0600

    aoc(2025): day 11 p1

--- 2025/10/p2/solution.go
+++ 2025/10/p2/solution.go
@@ -2,13 +2,111 @@ package p2
 
 import (
 	"fmt"
-	// "github.com/drcrees/aoc/helpers"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/drcrees/aoc/helpers"
 )
+
+type Machine struct {
+	id       int
+	config   uint
+	buttons  []uint
+	buttons2 []Button
+	joltages []uint
+}
+
+type Button struct {
+	indices []uint
+}
 
 func Solve() {
 	fmt.Println("--- 10-2 ---")
 
-	// strs := helpers.ReadLines("./2025/10/p2/input")
-	result := 0
+	strs := helpers.ReadLines("./2025/10/p2/input")
+	machines := parseMachines(strs)
+
+	var result int
+	for _, m := range machines {
+		fmt.Println(m)
+	}
+
 	fmt.Printf("Result: %d\n", result)
+}
+
+func fewestPresses(m Machine) int {
+	c := combos(m.buttons)
+
+	for _, subset := range c {
+		var value uint
+		for _, button := range subset {
+			value ^= button
+		}
+		if value == m.config {
+			return len(subset)
+		}
+	}
+	return 0
+}
+
+func combos(buttons []uint) (subsets [][]uint) {
+	length := uint(len(buttons))
+	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+		var subset []uint
+		for object := range length {
+			if (subsetBits>>object)&1 == 1 {
+				subset = append(subset, buttons[object])
+			}
+		}
+		subsets = append(subsets, subset)
+	}
+	sort.Slice(subsets, func(a, b int) bool {
+		return len(subsets[a]) < len(subsets[b])
+	})
+	return subsets
+}
+
+func parseMachines(strs []string) []Machine {
+	p1 := regexp.MustCompile(`[\.|#]+`)
+	p2 := regexp.MustCompile(`\(\S+\)+`)
+	p3 := regexp.MustCompile(`{(\S+)}`)
+
+	var machines []Machine
+	for i, str := range strs {
+		s1 := p1.FindString(str)
+		s2 := p2.FindAllString(str, -1)
+		s3 := p3.FindString(str)
+
+		var config uint
+		for i := range s1 {
+			if s1[i] == '#' {
+				config += (1 << i)
+			}
+		}
+
+		var buttons []uint
+		var buttons2 []Button
+		for _, bs := range s2 {
+			var button uint
+			var indices []uint
+			for _, s := range strings.Split(bs[1:len(bs)-1], ",") {
+				v, _ := strconv.Atoi(s)
+				button ^= (1 << v)
+				indices = append(indices, uint(v))
+			}
+			buttons = append(buttons, button)
+			buttons2 = append(buttons2, Button{indices})
+		}
+
+		var joltages []uint
+		for _, s := range strings.Split(s3[1:len(s3)-1], ",") {
+			v, _ := strconv.Atoi(s)
+			joltages = append(joltages, uint(v))
+		}
+		machines = append(machines, Machine{i + 1, config, buttons, buttons2, joltages})
+	}
+
+	return machines
 }
---
+++ 2025/11/p2/solution.go
@@ -0,0 +1,52 @@
+package p2
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/drcrees/aoc/helpers"
+)
+
+type Graph struct {
+	edges map[string][]string
+}
+
+func Solve() {
+	fmt.Println("--- 11-2 ---")
+
+	strs := helpers.ReadLines("./2025/11/p2/input")
+	graph := buildGraph(strs)
+
+	var result int
+	result = graph.paths("you")
+
+	fmt.Printf("Result: %d\n", result)
+}
+
+func (g *Graph) paths(current string) int {
+	paths := 0
+	edges := g.edges[current]
+
+	if current != "out" && len(edges) == 0 {
+		return 0
+	}
+
+	if current == "out" {
+		return 1
+	}
+
+	for _, edge := range edges {
+		paths += g.paths(edge)
+	}
+	return paths
+}
+
+func buildGraph(strs []string) Graph {
+	graph := Graph{edges: make(map[string][]string)}
+	for _, str := range strs {
+		parts := strings.Split(str, ": ")
+		edges := strings.Split(parts[1], " ")
+		graph.edges[parts[0]] = edges
+	}
+	return graph
+}
