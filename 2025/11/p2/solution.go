62bb4b78dc680f843da280f269a4262b4eb0d956
drcrees <dylan@crees.io>
2025-12-13 16:05:43 -0600

    aoc(2025): day 10 part 2

--- 2025/11/p2/solution.go
+++ 2025/11/p2/solution.go
@@ -14,31 +14,49 @@ type Graph struct {
 func Solve() {
 	fmt.Println("--- 11-2 ---")
 
+	visited = make(map[node]int)
+	paths = [][]string{}
+
 	strs := helpers.ReadLines("./2025/11/p2/input")
 	graph := buildGraph(strs)
 
 	var result int
-	result = graph.paths("you")
+	result = graph.paths("svr", "out", false, false)
 
 	fmt.Printf("Result: %d\n", result)
 }
 
-func (g *Graph) paths(current string) int {
-	paths := 0
+type node struct {
+	name string
+	dac  bool
+	fft  bool
+}
+
+var visited map[node]int
+var paths [][]string
+
+func (g *Graph) paths(current string, destination string, dac bool, fft bool) int {
+	count := 0
 	edges := g.edges[current]
 
-	if current != "out" && len(edges) == 0 {
+	node := node{current, dac, fft}
+
+	if current == destination {
+		if dac && fft {
+			return 1
+		}
 		return 0
 	}
 
-	if current == "out" {
-		return 1
+	if res, ok := visited[node]; ok {
+		return res
 	}
 
 	for _, edge := range edges {
-		paths += g.paths(edge)
+		count += g.paths(edge, destination, dac || edge == "dac", fft || edge == "fft")
 	}
-	return paths
+	visited[node] = count
+	return count
 }
 
 func buildGraph(strs []string) Graph {
@@ -49,4 +67,10 @@ func buildGraph(strs []string) Graph {
 		graph.edges[parts[0]] = edges
 	}
 	return graph
+}
+
+func (g *Graph) print() {
+	for node, edge := range g.edges {
+		fmt.Printf("%s -> %s\n", node, edge)
+	}
 }
